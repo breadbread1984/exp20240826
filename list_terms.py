067595eb5ab785cebd95d633534f1389865565ce
breadbread1984 <breadbread1984@163.com>
2024-08-26 09:03:38 +0800

    update

---
+++ list_terms.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+from absl import flags, app
+import pickle
+from chains import term_chain
+
+FLAGS = flags.FLAGS
+
+def add_options():
+  flags.DEFINE_enum('model', default = 'llama3', enum_values = {'llama3', 'qwen2'}, help = 'model')
+
+def main(unused_argv):
+  chain = term_chain(FLAGS.model)
+  terms = chain.invoke({'field': 'sulfide electrolyte'})
+  with open('terms.pkl', 'wb') as f:
+    f.write(pickle.dumps(terms))
+
+if __name__ == "__main__":
+  add_options()
+  app.run(main)
+
