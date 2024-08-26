3c1f6f2c5fdff444e328805046d2d5144f4ec247
breadbread1984 <breadbread1984@163.com>
2024-08-26 01:16:38 +0000

    update

--- prompts.py
+++ prompts.py
@@ -7,15 +7,14 @@ from langchain_core.prompts.prompt import PromptTemplate
 
 def get_terms_template(tokenizer):
   class Terms(BaseModel):
-    terms: List[str] = Field('a list of professional terms in field given by user.')
+    terms: List[str] = Field('a list of professional terms in field of {field}.')
   parser = JsonOutputParser(pydantic_object = Terms)
   instructions = parser.get_format_instructions()
   instructions = instructions.replace('{','{{')
   instructions = instructions.replace('}','}}')
-  system_prompt = f"Please list all professional terms in the field given by user. \n\n{instructions}"
+  user_prompt = "Please list all professional terms in the field of {field}. \n\n%s" % instructions
   messages = [
-    {'role': 'system', 'content': system_prompt},
-    {'role': 'user', 'cotent': '{field}'}
+    {'role': 'user', 'content': user_prompt},
   ]
   prompt = tokenizer.apply_chat_template(messages, tokenize = False, add_generation_prompt = True)
   template = PromptTemplate(template = prompt, input_vairables = ['field'])
