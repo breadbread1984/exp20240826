7e54bf8f9e280763792a2e28047644f90a393c18
breadbread1984 <breadbread1984@163.com>
2024-08-28 10:57:07 +0800

    update

--- create_nym_web.py
+++ create_nym_web.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import pickle
-import wikipediaapi as wapi
+import wikipedia as wiki
 from absl import app, flags
 
 FLAGS = flags.FLAGS
@@ -29,38 +29,33 @@ def add_edge(fp, node1, node2, mode = 'cat_term'):
 def main(unused_argv):
   with open('terms.pkl', 'rb') as f:
     terms = pickle.loads(f.read())
-  wiki = wapi.Wikipedia(user_agent = 'wikpediaapi', language = 'en')
   fp = open(FLAGS.output, 'w')
   open_list = list()
   close_list = set()
   for term in terms['properties']['terms']:
-    page = wiki.page(term)
-    if not page.exists():
+    try:
+      page = wiki.page(term)
+    except:
       print(f'can\'t find term {term} on wikipedia!')
       continue
-    categories = page.categories
-    for title in sorted(categories.keys()):
-      category_name = title.replace('Category:','')
-      open_list.append((category_name, 1))
-      add_non_term_node(fp, category_name)
+    categories = [category for category in page.categories if len(category.split(' ')) <= 4]
+    for category in sorted(categories):
+      open_list.append((category, 1))
+      add_non_term_node(fp, category)
   while (len(open_list)):
-    node, d = open_list.pop(0)
-    if node in close_list: continue
-    close_list.add(node)
-    category = wiki.page(f"Category:{node}")
-    if not category.exists(): continue
-    for category_or_term in category.categorymembers.values():
-      if category_or_term.ns == wapi.Namespace.CATEGORY:
-        subcategory = category_or_term
-        category_name = subcategory.title.replace('Category:', '')
-        if d < FLAGS.depth:
-          open_list.append((category_name, d + 1))
-        add_non_term_node(fp, category_name)
-        add_edge(fp, node, category_name, 'cat_cat')
-      if category_or_term.ns == wapi.Namespace.MAIN:
-        page = category_or_term
-        add_term_node(fp, page.title)
-        add_edge(fp, node, page.title, 'cat_term')
+    category, d = open_list.pop(0)
+    if category in close_list: continue
+    close_list.add(category)
+    try:
+      page = wiki.page(f"Category:{category}")
+    except:
+      continue
+    for term in page.links():
+      add_term_node(fp, term)
+      add_edge(fp, category, term, 'cat_term')
+    for subcategory in [subcategory for subcategory in page.categories if len(subcategory.split(' ')) <= 4]:
+      add_non_term_node(fp, category_name)
+      add_edge(fp, category, subcategory, 'cat_cat')
   fp.close()
 
 if __name__ == "__main__":
