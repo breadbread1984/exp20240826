2dd5e81c9f5cfaf7dddad11d974311c970149e1a
breadbread1984 <breadbread1984@163.com>
2024-08-29 08:40:05 +0000

    update

--- batch_entity.sh
+++ batch_entity.sh
@@ -6,5 +6,5 @@ mkdir entities
 for f in documents/*.txt
 do
 	filename=`basename "$f"`
-	python3 list_entities.py --input "$f" --output "entities/$filename"
+	python3 list_entities.py --model qwen2 --input "$f" --output "entities/$filename".pkl
 done
