f684858116684cc1bd8121a3556afba3ed2df948
breadbread1984 <breadbread1984@163.com>
2024-08-29 10:30:55 +0800

    update

--- spacy_ner.py
+++ spacy_ner.py
@@ -13,7 +13,7 @@ def main(unused_argv):
   nlp = spacy.load("en_core_web_sm")
   with open(FLAGS.input, 'r') as f:
     tokens = nlp(f.read())
-  results = [(token[0], token[1], token[1] + len(token[0])) for token in tokens]
+  results = [(token.text, token.pos, token.pos + len(token.text)) for token in tokens]
   with open(FLAGS.output, 'w') as f:
     f.write(json.dumps(results))
 
