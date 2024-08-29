3127eec556c114f333851b97bc1c045e0de28edc
breadbread1984 <breadbread1984@163.com>
2024-08-29 16:08:53 +0800

    update

--- chains.py
+++ chains.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from models import Llama3, Llama3_FA2, Qwen2, Qwen2_FA2
-from prompts import get_terms_template
+from prompts import get_terms_template, get_entity_template
 
 def term_chain(model = 'llama3'):
   model = {
@@ -13,3 +13,12 @@ def term_chain(model = 'llama3'):
   chain = prompt | llm | parser
   return chain
 
+def entity_chain(model = 'llama3'):
+  model = {
+    'llama3': Llama3_FA2,
+    'qwen2': Qwen2_FA2
+  }[model]
+  tokenizer, llm = model(True)
+  prompt, parser = get_entity_template(tokenizer)
+  chain = prompt | llm | parser
+  return chain
