7ed5f79de3a5af9745e0fd1b31e8a442f8bf9f68
breadbread1984 <breadbread1984@163.com>
2024-08-30 10:29:48 +0800

    update

--- create_vector_db_zy.py
+++ create_vector_db_zy.py
@@ -25,9 +25,10 @@ def main(unused_argv):
         terms.add(record['term']['formula'])
         if '-' in record['term']['formula']:
           mats = record['term']['formula'].split('-')
-          terms.add(mats)
           if len(mats) == 2:
             terms.add('-'.join(mats.reverse()))
+          for mat in mats:
+            terms.add(mat)
 
   embeddings = HuggingFaceEmbeddings(model_name = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
   vectordb = Chroma.from_documents(documents = [Document(page_content = term) for term in terms], embedding = embeddings, persist_directory = FLAGS.output)
